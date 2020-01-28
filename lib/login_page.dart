f6d716c0082c0c5e4992be2c7fcff7a9aabec187
Thilina LB <crazythili93@gmail.com>
2020-01-28 20:23:34 +0530

    validate_loginform

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,8 +7,23 @@ class LoginPage extends StatefulWidget{
 
   class _LoginPageState extends State<LoginPage>{
 
+  final formkey = new GlobalKey<FormState>();
+
   String _email;
   String _password;
+
+  void validateAndSave(){
+    final form = formkey.currentState;
+
+    if(form.validate()){
+      form.save();
+      print('Form is Valid.. Email: $_email, password: $_password');
+    }
+    else{
+      print('Form is Invalid..Email: $_email, password: $_password');
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +35,25 @@ class LoginPage extends StatefulWidget{
       body: new Container(
         padding: EdgeInsets.all(16.0),
         child: new Form(
+          key: formkey,
           child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               new TextFormField(
                 decoration: new InputDecoration(labelText: 'Email'),
+                validator: (value) => value.isEmpty ?'Email can\'t be empty..':null,
+                onSaved: (value) => _email = value,
               ),
               new TextFormField(
                 decoration: new InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) => value.isEmpty ?'Password can\'t be empty..':null,
+                onSaved: (value) => _password = value,
+              ),
+              new RaisedButton(
+                child: new Text('Login', style: new TextStyle(fontSize: 20.0),),
+                onPressed: validateAndSave,
               )
-
             ],
           ),
         ),
