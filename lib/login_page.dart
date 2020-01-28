aed50458f35ab14e2deabf27b573d8898a33e72d
Thilina LB <crazythili93@gmail.com>
2020-01-28 16:58:56 +0530

    make_Statelesswidget_main_file_and_loginPage

---
+++ lib/login_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Fluter login demo'),
+      ),
+      body: new Container(
+        child: new Text('Hello world'),
+      ),
+    );
+  }
+  
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,21 @@
 import 'package:flutter/material.dart';
+import './login_page.dart';
 
-//void main(){
-// runApp(MyApp());
-//}
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-
- @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return MyAppState();
-  }
-
+void main(){
+  runApp(new MyApp());
 }
-class MyAppState extends State<MyApp>{
-  var questionIndex = 0;
-
-  void answerQuestion() {
-    setState(() {
-      questionIndex = questionIndex + 1;
-    });
-    print(questionIndex);
-  }
+class MyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'What\'s your favarite color??',
-      'what\'s your favarite animal??',
-    ];
-    // TODO: implement build
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('My First App'),
-          ),
-          body: Column(
-            children: [
-              Text(questions.elementAt(questionIndex),),
-              RaisedButton(
-                child: Text('Answer 1'),
-                onPressed: answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('Answer 2'),
-                onPressed: () => print('Answer 2 chosen'),
-              ),
-              RaisedButton(
-                child: Text('Answer 3'),
-                onPressed: () {
-                  print('Answer 3 chosen');
-                },
-              ),
-            ],
-          )),
+
+    return new MaterialApp(
+      title: 'flutter login demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new LoginPage(),
     );
   }
+
 }
