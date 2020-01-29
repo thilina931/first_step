5a16e9d8f0834d1c4d3d0a1a67dc30255c8bc45e
Thilina LB <crazythili93@gmail.com>
2020-01-29 11:33:58 +0530

    homepage_with_user_email

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home extends StatefulWidget {
+  const Home({
+    Key key,
+    @required this.user
+  }) : super(key: key);
+  final FirebaseUser user;
+
   @override
   _HomeState createState() => _HomeState();
 }
@@ -9,9 +16,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-    title: Text('Home'),
-    ),
+      appBar: AppBar(
+        title: Text('Home ${widget.user.email}'),
+      ),
     );
   }
 }
